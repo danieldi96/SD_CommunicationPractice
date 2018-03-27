09538f15a78ac9d116a36ff94c720f515651cba4
danieldi96 <danieldi1996@gmail.com>
2018-03-28 01:56:14 +0200

    Map Done

--- mapper.py
+++ mapper.py
@@ -1,27 +1,18 @@
-
 import sys
 
-#Mapper 
-def map(num_line, line):
-	words={}
-    line = line.lower()
-    line = line.replace('.', '').replace(',', '').replace(':', '')
-	word_split=line.split(" ")
-	for word in word_split:
-		if word.endwith("\n"):
-			counts[word]=counts.get(word,0) + 1		#Get -> Getordefault
-		print (words, 1)
-	return words
-'''
-def mapper(line):
-    """ Map function definition. Splits the lines and generates key-value for
-        each word.
-    """
-    counts = {}
-    line = line.lower()
-    line = line.replace('.', '').replace(',', '').replace(':', '')
-    words = line.split()
-    for word in words:
-        counts[word] = counts.get(word, 0) + 1
-    return counts
-'''
+# Mapper
+
+
+class Mapper(object):
+    _tell = []
+    _ask = ["map"]
+
+    def map(self, num_line, line):
+		words = {}
+		line = line.lower()
+		line = line.replace('.', '').replace(',', '').replace(':', '').replace('\n','').replace('\t','')
+		word_split = line.split(" ")
+		for word in word_split:
+			words[word] = words.get(word, 0) + 1  # Get -> Getordefault
+			print (words, 1)
+		return words
--- master.py
+++ master.py
@@ -1,31 +1,30 @@
-from pyactor.context import set_context, create_host, shutdown
-import mapper
+# -*- coding: utf-8 -*-
 
-def readFile():
-	i = 0
-	with open("fichero.txt", "r") as file_open:
- 		map.map(i,file_open.readLine()) #map recibe el numero de lunea y la linea entera
-		i+=1
-	file_open.close()
+from pyactor.context import set_context, create_host, shutdown, serve_forever
+import io
+import mapper as mp
+global dictionary
+class Master(object):
+    _tell = []
+    _ask = ['readFile']
 
-class Echo(object):
-    _tell = ['echo']
-    _ask = ['get_msgs']
-
-    def __init__(self):
-        self.msgs = []
-
-    def echo(self, msg):
-        print msg
-        self.msgs.append(msg)
-
-    def get_msgs(self):
-        return self.msgs
+    def readFile(self, slave):
+        i = 0
+        file = io.open("fichero.txt", "r", encoding="utf-8")
+        for line in iter(lambda: file.readline(), ""):
+            dictionary[i] = slave.map(i, line)
+            i += 1
+        file.close()
 
 
 if __name__ == "__main__":
+    dictionary = {}
     set_context()
-    host = create_host('http://127.0.0.1:1277/')
-
-    e1 = host.spawn('echo1', Echo)
+    host = create_host("http://127.0.0.1:1551")
+    master1 = host.spawn('Master1', Master)
+    for slaves in
+        slave1 = host.spawn('Slave1', 'mapper/Mapper')
+        master1.readFile(slave1)
+    print "\n==================================\n"
+    print dictionary
     serve_forever()
