96bb730c05ffcdf18868477067351c947b23c7ce
danieldi96 <danieldi1996@gmail.com>
2018-04-12 23:20:53 +0200

    Finished

--- project/mapper.py
+++ project/mapper.py
@@ -1,37 +1,37 @@
 # -*- coding: utf-8 -*-
 from pyactor.context import set_context, create_host, Host, sleep, shutdown
 import io, sys, urllib, collections, os
-global num_mappers
 # Mapper
 class Mapper(object):
     _tell = ['start','map']             #Methods asincronous
     _ask = []
     _ref = ['map']              #Methods which we get a reference
-    '''
-    def cw(self, word_split):
-        for word in word_split:
-            self.words["Paraula"] = self.words.get("Paraula", 0) + 1
-    '''
+
     def wc(self, word_split):
         for word in word_split:
             if word != "":
+                if word.endswith("-") or word.startswith("-"):
+                    word.replace("-", "")
                 self.words[word] = self.words.get(word, 0) + 1  # Get -> Getordefault
+        return self.words
 
     def map(self, remote_reducer, ip_files, program, id_mapper):
         self.words = {}
         try:
-            text = open(("x%s%s"%(chr((id_mapper/26)+97), chr((id_mapper%26)+97))), "r").read().lower()
+            self.text = open(("x%s%s"%(chr((id_mapper/26)+97), chr((id_mapper%26)+97))), "r").read().lower()
         except IOError:
             print "\n\nERROR. No se puede abrir el archivo desde el mapper.\n"
         finally:
-            text = text.replace('.', '').replace(',', '').replace(':', '').replace('\n',' ').replace('\t','').replace(';','').replace("!", "").replace("?", "").replace("-","")
-            word_split = text.split()
+            for char in ".,:;!?()[]'\t'":
+                self.text = self.text.replace(char, "")
+            self.text = self.text.replace('\n',' ')
+            word_split = self.text.split()
             if program == 'WC':
-                self.wc(word_split)
-                remote_reducer.wc(self.words)
+                remote_reducer.wc(self.wc(word_split))
             elif program == 'CW':
+                key = "Word"
                 for word in word_split:
-                    self.words["Paraula"] = self.words.get("Paraula", 0) + 1
+                    self.words[key] = self.words.get(key, 0) + 1
                 remote_reducer.cw(self.words)
             else:
                 print "\n\tEl programa seleccionado no se encuentra disponible.\n"
@@ -49,7 +49,6 @@ if __name__ == "__main__":
     finally:
         host = create_host('http://127.0.0.1:190%s/' % str(id_mapper))
         print "\n\tCargando...\n"
-        # Reference to hosts
         remote_mapper = host.lookup_url("http://127.0.0.1:160%s/Mapper"%str(id_mapper), 'Mapper', 'mapper')
         remote_master = host.lookup_url("http://127.0.0.1:1500/regis", 'Registry', 'master')
         remote_master.bind("Mapper_%s"%str(id_mapper))
--- project/reduce.py
+++ project/reduce.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from pyactor.context import set_context, create_host, Host, sleep, shutdown
 import time
 import collections
@@ -11,7 +12,6 @@ class Reducer(object):
     def start(self, nmappers):
         self.finished = nmappers
         self.dicc = {}
-        self.res_cw = 0
 
     def startCrono(self):
         self.timeinit = time.time()
@@ -19,29 +19,24 @@ class Reducer(object):
     def stopCrono(self):
         timeend = time.time()
         timeend = timeend - self.timeinit
-        print "\n\n\tHa tardado %s segundos" % str(timeend)
+        print "\n\n\tHa tardado %s segundos" % timeend
 
     def shuffle(self, dic_map):
         for key in dic_map.keys():
-            if key in self.dicc.keys():
-                self.dicc[key] = self.dicc.get(key, 0) + dic_map[key]      #si ya esta en el diccionario le sumamos 1
-            else:
-                self.dicc[key] = dic_map[key]           #si no esta, anadimos la clave al nuevo diccionario con las ocurrencias de esta en la linea
+            self.dicc[key] = self.dicc.get(key, 0) + dic_map[key]      #si ya esta en el diccionario le sumamos 1, si no está le podrá el valor de 1
 
     def wc(self, dic_map):
         self.shuffle(dic_map)
         self.finished -= 1
         if self.finished == 0:
-            print "\n\tLas palabras repetidas son: (<palabra, #veces>)"
             for key in self.dicc.keys():
-                line_dic = key+" : "+str(self.dicc[key])
-                print line_dic
-            print "\n\n\tPuede encontrar las palabras repetidas en: ./files/palabras_repetidas.txt"
+                print "%s : %s" % (key, self.dicc[key])
+            print "\n\n\tLas palabras repetidas son: (<palabra, #veces>)"
             self.stopCrono()
 
     def cw(self, dic_map):
         self.shuffle(dic_map)
         self.finished -= 1
         if self.finished == 0:
-            print "\n\n\t El archivo tiene en total %s palabras." % self.dicc["Paraula"]
+            print "\n\n\t El archivo tiene en total %s palabras." % self.dicc["Word"]
             self.stopCrono()
