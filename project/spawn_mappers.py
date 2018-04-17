a51c568fa7d6159eec6b60a6fd66cc7fddca18bd
danieldi96 <danieldi1996@gmail.com>
2018-04-18 00:40:50 +0200

    Practice Finished

--- project/spawn_mappers.py
+++ project/spawn_mappers.py
@@ -6,11 +6,12 @@ if __name__ == "__main__" :
     try:
         mappers_total = int(sys.argv[1])
         ip = str(sys.argv[2])
+        ip_sv = str(sys.argv[3])
     except IndexError, SyntaxError:
         print "\n\tpython spawn_mappers.py [num_mappers_total] [ip_servidor_archivos*] \n\n\t* si ip_servidor_archivos es 'localhost' = 127.0.0.1\n"
     finally:
         print "\nEjecutando..."
         for i in range(0, mappers_total):
-            os.system("gnome-terminal -e 'bash -c \"python mapper.py %s %s; exec bash\"'" % (i, ip))
+            os.system("gnome-terminal -e 'bash -c \"python mapper.py %s %s %s; exec bash\"'" % (i, ip, ip_sv))
             #os.system("python mapper.py %s %s &" % (i, ip))
-        os.system("gnome-terminal -e 'bash -c \"python reduce.py %s; exec bash\"'" % ip)
+        os.system("gnome-terminal -e 'bash -c \"python reduce.py %s %s; exec bash\"'" % (ip, ip_sv))
